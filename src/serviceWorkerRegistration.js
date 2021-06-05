d49c63ee8abe37925f7f097d6e55200499abf3ba
hosnipogi <hosnibona@gmail.com>
2021-06-06 03:43:45 +0800

    removed test alert from service worker

--- src/serviceWorkerRegistration.js
+++ src/serviceWorkerRegistration.js
@@ -23,7 +23,6 @@ const isLocalhost = Boolean(
 export function register(config) {
   if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
     // The URL constructor is available in all browsers that support SW.
-    alert('here okay');
     const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
     if (publicUrl.origin !== window.location.origin) {
       // Our service worker won't work if PUBLIC_URL is on a different origin
@@ -52,8 +51,6 @@ export function register(config) {
         registerValidSW(swUrl, config);
       }
     });
-  } else {
-    alert('oh no');
   }
 }
 
